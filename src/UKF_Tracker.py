6996aadd5b4676de538a59ce4cc98ec5021bc92a
b-enguin <bkben64@gmail.com>
2024-03-27 16:52:16 +0800

    Update UKF Tracker

--- src/UKF_Tracker.py
+++ src/UKF_Tracker.py
@@ -25,18 +25,11 @@ from stonesoup.types.state import GaussianState
 from stonesoup.types.update import GaussianStateUpdate
 from stonesoup.updater.kalman import UnscentedKalmanUpdater
 import rospy
-# import rclpy
 from geometry_msgs.msg import Vector3, Quaternion, Point
-# from rclpy.duration import Duration
-# from rclpy.node import Node
 from bb_msgs.msg import DetectedObject, DetectedObjects
 from stonesoup.plotter import Plotter
-
 from std_msgs.msg import ColorRGBA
-# from tf2_ros import Buffer, TransformListener
 from visualization_msgs.msg import Marker, MarkerArray
-# from tf2_geometry_msgs import PoseStamped as TF2PoseStamped
-
 from sensor_msgs.msg import CompressedImage
 from cv_bridge import CvBridge, CvBridgeError
 
@@ -44,15 +37,13 @@ from cv_bridge import CvBridge, CvBridgeError
 class UKF_Tracker():
 
     def __init__(self, node_name: str) -> None:
-        # Object Movement Model
-        # super().__init__(node_name)
         rospy.init_node(node_name)
 
-        # self.get_logger().info("Starting: " + node_name)
-        # self.declare_parameter("filtered_topic", "/asv4/vision/external/fused_detections")
         rospy.loginfo("Starting: " + node_name)
-        rospy.set_param("filtered_topic", "/auv4/vision/external/fused_detections")
+        rospy.set_param("filtered_topic",
+                        "/auv4/vision/external/fused_detections")
 
+        # Object Movement Model
         self.transition_model = CombinedLinearGaussianTransitionModel(
             [
                 RandomWalk(0.1),  # x
@@ -75,9 +66,13 @@ class UKF_Tracker():
         self.updater = UnscentedKalmanUpdater(self.measurement_model)
 
         # Track Deleter
-        covar_deleter = CovarianceBasedDeleter(covar_trace_thresh=1.5)  # Higher Value Longer Persistence
-        time_deleter = UpdateTimeDeleter(timedelta(seconds=30)) # Time Since Last Update
-        self.deleter = CompositeDeleter([covar_deleter, time_deleter], intersect=False) # Any fail will cause deletion
+        covar_deleter = CovarianceBasedDeleter(
+            covar_trace_thresh=1.5)  # Higher Value Longer Persistence
+        time_deleter = UpdateTimeDeleter(
+            timedelta(seconds=30))  # Time Since Last Update
+        # Any fail will cause deletion
+        self.deleter = CompositeDeleter(
+            [covar_deleter, time_deleter], intersect=False)
 
         # Hypothesiser
         prob_detect = 0.9
@@ -111,14 +106,23 @@ class UKF_Tracker():
         self.tracks, self.all_tracks = dict(), dict()
 
         # Detected Object Sub
-        self.detection_sub = rospy.Subscriber("/auv4/vision/external/detected_filtered", DetectedObjects, self.detection_callback)
-        #self.detection_sub = self.create_subscription(
-        #    DetectedObjectsStamped,
-        #    '/asv4/vision/lidar/detected_stamped',
-        #    self.detection_callback,
-        #    10
-        #)
+        self.detection_sub = rospy.Subscriber(
+            "/auv4/vision/external/detected_filtered", DetectedObjects, self.detection_callback)
+        
+        # Detected Object Pub
+        self.detection_pub = rospy.Publisher(
+            "/auv4/vision/UKF/detected", 
+            DetectedObjects,
+            10
+        )
 
+        # Pub Timer
+        self.pub_timer = rospy.Timer(
+            rospy.Duration(1),
+            self.pub_detections
+        )
+
+        # Plotting
         plt.ioff()
         self.fig = plt.figure()
         self.ax = self.fig.add_subplot()
@@ -126,36 +130,28 @@ class UKF_Tracker():
         self.plotter = Plotter()
         self.plotCount = 0
 
-        # Marker Pub
-        #self.publisher_ = self.create_publisher(
-        #    MarkerArray, '/stereo_visualization', 1)
-
         # Plot Pub
-        self.publisher_plot = rospy.Publisher("/filter_visualisation/iamge_raw/compressed", CompressedImage)
-        #self.publisher_plot = self.create_publisher(
-        #    CompressedImage, '/filter_visualisation/image_raw/compressed', 10)
-        #self.filtered_topic = self.get_parameter("filtered_topic")\
-        #                          .get_parameter_value().string_value
-        #self.tracked_objects_pub = self.create_publisher(
-        #    DetectedObjectsStamped, self.filtered_topic, 10)
-        #self._tf_buffer = Buffer()
-        #self._tf_listener = TransformListener(self._tf_buffer, self)
+        self.publisher_plot = rospy.Publisher(
+            "/filter_visualisation/image_raw/compressed", 
+            CompressedImage)
 
         self.bridge = CvBridge()
 
     def detection_callback(self, detections: DetectedObjects):
-        #self.get_logger().info("Det")
+        # self.get_logger().info("Det")
         start = time.time()
         if len(detections.detected) > 0:
             # Process Detections
             names = set([obj.name for obj in detections.detected])
             for name in names:
                 if self.tracks.get(name) is None:
-                    rospy.loginfo("Creating new Track for {} objects".format(name))
+                    rospy.loginfo(
+                        "Creating new Track for {} objects".format(name))
                     self.tracks[name] = set()
                     self.all_tracks[name] = set()
 
-            grouped = {x: [y for y in detections.detected if y.name == x] for x in names}
+            grouped = {x: [y for y in detections.detected if y.name == x]
+                       for x in names}
             for name, objects in grouped.items():
                 measurement_set = set()
 
@@ -163,17 +159,21 @@ class UKF_Tracker():
                 for obj in objects:
                     coords = np.array(obj.world_coords)
                     if len(coords) != 0:
-                        timestamp = datetime.fromtimestamp(obj.header.stamp.to_sec())
+                        timestamp = datetime.fromtimestamp(
+                            obj.header.stamp.to_sec())
                         measurement_set.add(Detection(
                             state_vector=StateVector(coords),
                             timestamp=timestamp,
-                            measurement_model=self.measurement_model))
+                            measurement_model=self.measurement_model,
+                            metadata={
+                                "frame":obj.header.frame_id
+                            }))
 
                 # Process Detections with JPDA UKF
                 if len(measurement_set) > 0:
                     hypotheses = self.data_associator.associate(self.tracks[name],
-                                                            measurement_set,
-                                                            timestamp)
+                                                                measurement_set,
+                                                                timestamp)
 
                     associated_measurements = set()
                     for track in self.tracks[name]:
@@ -185,17 +185,23 @@ class UKF_Tracker():
                             if not hypothesis:
                                 posterior_states.append(hypothesis.prediction)
                             else:
-                                posterior_state = self.updater.update(hypothesis)
+                                posterior_state = self.updater.update(
+                                    hypothesis)
                                 posterior_states.append(posterior_state)
-                                associated_measurements.add(hypothesis.measurement)
-                            posterior_state_weights.append(hypothesis.probability)
+                                associated_measurements.add(
+                                    hypothesis.measurement)
+                            posterior_state_weights.append(
+                                hypothesis.probability)
 
-                        means = StateVectors([state.mean for state in posterior_states])
-                        covars = np.stack([state.covar for state in posterior_states], axis=2)
+                        means = StateVectors(
+                            [state.mean for state in posterior_states])
+                        covars = np.stack(
+                            [state.covar for state in posterior_states], axis=2)
                         weights = np.asarray(posterior_state_weights)
 
                         # Reduce mixture of states to one posterior estimate Gaussian.
-                        post_mean, post_covar = gm_reduce_single(means, covars, weights)
+                        post_mean, post_covar = gm_reduce_single(
+                            means, covars, weights)
 
                         # Add a Gaussian state approximation to the track.
                         track.append(GaussianStateUpdate(
@@ -204,7 +210,8 @@ class UKF_Tracker():
                             track_hypotheses[0].measurement.timestamp))
 
                         # Deletion and Initiation
-                    self.tracks[name] -= self.deleter.delete_tracks(self.tracks[name])
+                    self.tracks[name] -= self.deleter.delete_tracks(
+                        self.tracks[name])
                     self.tracks[name] |= self.initiator.initiate(measurement_set - associated_measurements,
                                                                  timestamp)
 
@@ -212,147 +219,37 @@ class UKF_Tracker():
 
             rospy.loginfo("Processed in %.4f seconds" % (time.time() - start))
 
-            # Plots
-            # Marker
-            # id=0
-            #markers = MarkerArray()
-            #marker = Marker()
-            #marker.header = obj.header
-            #marker.scale = Vector3(x=1.0, y=1.0, z=1.0)
-            #marker.pose.orientation = Quaternion(
-            #    w=0.707, x=0.0, y=0.0, z=0.0)
-            #marker.action = Marker.DELETEALL
-            #markers.markers.append(deepcopy(marker))
-            #marker.action = Marker.ADD
             plot_marker = None
             plot_color = None
-            #id = 1
-
-            #output = DetectedObjectsStamped()
-            #output.header = detections.header
 
             for name in names:
                 for track in self.tracks[name]:
                     coords = track.state_vector.flatten()
 
                     # Using back of Track id
-                    rospy.loginfo(f"Tracking {coords} {name}@{track.id.split('-')[-1]}")
-
-            #        color = ColorRGBA()
-                    if "round" in name:
-            #            marker.type = Marker.SPHERE
-                        plot_marker = "o"
-                    else:
-            #            marker.type = Marker.CYLINDER
-                        plot_marker = "^"
+                    rospy.loginfo(
+                        f"Tracking {coords} {name}@{track.id.split('-')[-1]}")
+                    
+                    plot_marker = "^"
 
                     if "green" in obj.name:
-            #            color.r = 0.0
-            #            color.g = 1.0
-            #            color.b = 0.0
                         plot_color = "green"
                     elif "red" in obj.name:
-            #            color.r = 1.0
-            #            color.g = 0.0
-            #            color.b = 0.0
                         plot_color = "red"
                     elif "black" in obj.name:
-            #            color.r = 0.0
-            #            color.g = 0.0
-            #            color.b = 0.0
                         plot_color = "black"
                     elif "orange" in obj.name:
-            #            color.r = 1.0
-            #            color.g = 0.64
-            #            color.b = 0.0
                         plot_color = "orange"
                     elif "white" in obj.name:
-            #            color.r = 1.0
-            #            color.g = 1.0
-            #            color.b = 1.0
                         plot_color = "white"
                     elif "rgb" in obj.name:
-            #            color.r = 0.0
-            #            color.g = 0.0
-            #            color.b = 1.0
                         plot_color = "blue"
                     else:
-            #            color.r = 1.0
-            #            color.g = 1.0
-            #            color.b = 1.0
                         plot_color = "gray"
-            #        color.a = 1.0
 
-            #        p = Point()
-            #        p.x = coords[0]
-            #        p.y = coords[1]
-            #        p.z = coords[2]
-
-            #        marker.color = color
-            #        marker.pose.position = p
-            #        marker.id = id
-            #        id += 1
-            #        marker.header.frame_id = "map_ned"
-
-            #        tracked_obj_msg = DetectedObject()
-            #        tracked_obj_msg.name = obj.name
-            #        tracked_obj_msg.world_coords = [
-            #            coords[0],
-            #            coords[1],
-            #            0.0,
-            #        ]
-            #        tracked_obj_msg.real_dims = [0.5, 0.5, 0.5]
-            #        tracked_obj_msg.move_coords = 2
-            #        tracked_obj_msg.tracker_confidence = [1]
-            #        # tracked_obj_msg.extra = [int(track.id.split('-')[-1])]
-            #        output.detected.append(tracked_obj_msg)
-
-                    # id += 1
-
-                    # TODO verify uncertainty marker
- #                   uncertainty_marker, text = self.get_uncertainty_marker(track, [0, 1, 2], color)
- #                   uncertainty_marker.id = id
- #                   uncertainty_marker.header.frame_id = "map_ned"
- #                   id += 1
- #                   text.header.frame_id = "map_ned"
- #                   text.id = id
- #                   text.pose.position = marker.pose.position
- #                   id += 1
-
-#                    markers.markers.append(deepcopy(marker))
-#                    markers.markers.append(deepcopy(uncertainty_marker))
-#                    markers.markers.append(deepcopy(text))
-#                    try:
-#                        tf2_pose = TF2PoseStamped()
-#                        tf2_pose.header = marker.header
-#                        tf2_pose.header.frame_id = 'map_ned'
-#                        tf2_pose.pose = marker.pose
-#                        target_pose = "asv4/base_link"
-#                        base_link = self._tf_buffer.transform(
-#                            tf2_pose, target_pose, Duration(seconds=0.0))
-#                        # base_link_coords = list(
-#                        #     attrgetter("x", "y", "z")(base_link.pose.position))
-#                        marker.pose.position = base_link.pose.position
-#                        marker.header.frame_id = target_pose
-#
-#                        # Plot Uncertainty Markers
-#                        uncertainty_marker.pose.position = base_link.pose.position
-#                        uncertainty_marker.header.frame_id = target_pose
-#                        text.pose.position = base_link.pose.position
-#                        text.header.frame_id = target_pose
-#
-#                        # print(marker)
-#                        markers.markers.append(deepcopy(marker))
-#                        markers.markers.append(deepcopy(uncertainty_marker))
-#                        markers.markers.append(deepcopy(text))
-#                    except Exception as e:
-#                        self.get_logger().warn(
-#                            f"Failed to convert to base_link {e}")
-#
                     # Plot on 2d map
-                    self.draw_uncertainty(track, [0, 1], self.ax, plot_color, plot_marker)
-
-#            self.tracked_objects_pub.publish(output)
+                    self.draw_uncertainty(
+                        track, [0, 1], self.ax, plot_color, plot_marker)
 
             # Prepare Plot Image
             self.fig.canvas.draw()
@@ -360,12 +257,24 @@ class UKF_Tracker():
             img_msg = self.bridge.cv2_to_compressed_imgmsg(graph_image)
             self.publisher_plot.publish(img_msg)
             self.ax.cla()
-            # TODO: Add Plot - CV - ROS Pub
 
-#            self.publisher_.publish(markers)
+    def pub_detections(self):
+        pub_objects = DetectedObjects()
+
+        for name, tracks_by_class in self.tracks.items():
+            for track in tracks_by_class:
+                obj = DetectedObject()
+                obj.header.frame_id = track[0].measurement.metadata["frame"]
+                obj.header.stamp.from_sec(track.timestamp.timestamp())
+                obj.world_coords = track.state_vector.flatten()
+                obj.name = name
+
+        self.detection_pub.publish(pub_objects)
+                
 
     def draw_uncertainty(self, track, mapping, ax, color, marker):
-        state, data, min_ind, max_ind, orient, w, v = self.calculate_uncertainty(track, mapping)
+        state, data, min_ind, max_ind, orient, w, v = self.calculate_uncertainty(
+            track, mapping)
         ax.scatter(data[0], data[1], color=color, marker=marker)
         ax.text(data[0], data[1], track.id.split('-')[-1], fontsize="x-small")
         ellipse = Ellipse(xy=state.mean[mapping[:2], 0],
@@ -374,39 +283,6 @@ class UKF_Tracker():
                           angle=np.rad2deg(orient), alpha=0.5,
                           color=color)
         ax.add_patch(ellipse)
-
-    def get_uncertainty_marker(self, track, mapping, color):
-        state, data, min_ind, max_ind, orient, w, v = self.calculate_uncertainty(track, mapping)
-        marker = Marker()
-        marker.scale = Vector3(
-            x=2 * np.sqrt(w[max_ind]),
-            y=2 * np.sqrt(w[min_ind]),
-            z=0.0)
-
-        # TODO fix angle for marker
-        marker.pose.orientation = Quaternion(
-            w=0.707, x=0.707, y=0.0, z=0.0)
-        marker.action = Marker.DELETEALL
-        marker.type = Marker.SPHERE
-        p = Point()
-        p.x = state.mean[mapping[0]]
-        p.y = state.mean[mapping[1]]
-        p.z = state.mean[mapping[2]]
-        color.a = 0.5
-        marker.color = color
-        marker.pose.position = p
-
-        text = Marker(text=track.id.split('-')[-1])
-        text.type = Marker.TEXT_VIEW_FACING
-        text.pose.position = marker.pose.position
-        text.pose.position.z += 1.0
-        text.scale.z = 5.0
-
-        return marker, text
-
-
-
-
 
     def calculate_uncertainty(self, track, mapping):
         HH = np.eye(track.ndim)[mapping, :]  # Get position mapping matrix
@@ -423,13 +299,14 @@ class UKF_Tracker():
             orient = np.array([0])
         return state, data, min_ind, max_ind, orient, w, v
 
+
 def main(args=None):
-    #rclpy.init(args=args)
+    # rclpy.init(args=args)
 
     ukf_tracker = UKF_Tracker(node_name="ukf_tracker")
     rospy.spin()
 
-    #rclpy.shutdown()
+    # rclpy.shutdown()
 
 
 if __name__ == '__main__':
