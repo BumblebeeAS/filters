c2c2bb6fccd2882cac7ef112ba7ef51879eee7f5
TroubleMaker <isabellalu01@gmail.com>
2024-11-06 01:46:17 +0800

    update acoustics

--- scripts/pinger_gate_detection.py
+++ scripts/pinger_gate_detection.py
@@ -13,6 +13,7 @@ from bb_perception_msgs.msg import (
 )
 from collections import deque
 from std_srvs.srv import SetBool 
+from collections import Counter
 
 """ pinger msg
 int32 doa
@@ -46,7 +47,7 @@ class GateStatusNode(Node):
         pinger_data = self.get_parameter("pinger_data").get_parameter_value().string_value
         
         # taking some doa to average for best result 
-        self.declare_parameter("doa_cluster_size", 5)
+        self.declare_parameter("doa_cluster_size", 10)
         self.doa_cluster_size = self.get_parameter("doa_cluster_size").get_parameter_value().integer_value
         self.get_logger().info(f"DOA cluster size set to: {self.doa_cluster_size}")
 
@@ -69,7 +70,11 @@ class GateStatusNode(Node):
             self.pinger_callback, 
             10,
         )
-
+    
+        # assuming 5m in front and facing gate straight head 
+        self.gate_left_threshold = (270,320) # red - white 
+        self.gate_middle_threshold = (325,40) # white - white 
+        self.gate_right_threshold = (45,75) # white - green
         # Publishers
         self.pub_gate_ = self.create_publisher(String, '/asv4/robotx/pinger_gate', 10)
         self.pub_status_ = self.create_publisher(Bool, '/asv4/robotx/pinger_gate_status', 10)
@@ -80,9 +85,9 @@ class GateStatusNode(Node):
         self.gate_detection = None
         self.vehicle_position = None
         self.pinger_data = None 
-        self.doa_values = deque(maxlen=self.doa_cluster_size) # store last N doa values 
-
-        # Timer to periodically check data and publish status
+        # self.doa_values = deque(maxlen=self.doa_cluster_size) # store last N doa values 
+        self.batch = []
+        
         #  self.timer = self.create_timer(0.5, self.process_data) # move to pinger callback
         self.is_active = False  # Start with the timer off
         self.get_logger().info(f"Pinger Gate Node Initialised, waiting for activation")
@@ -94,71 +99,63 @@ class GateStatusNode(Node):
     def odom_callback(self, msg):
         self.vehicle_position = msg.pose.pose.position
         # self.get_logger().info(f'Received odometry data.'throttle_duration_sec=2.0)
+        
+    def get_gate_from_doa(self,doa):
+        if self.gate_middle_threshold[0] >= doa and doa <= self.gate_middle_threshold[1] : 
+           return  "gate_middle"
+        elif self.gate_right_threshold[0] <= doa <= self.gate_right_threshold[1] : 
+            return "gate_right" 
+        elif self.gate_left_threshold[0] <= doa <= self.gate_left_threshold[1] : 
+            return "gate_left" 
+        else:
+            return "Invalid"      
 
     def pinger_callback(self, msg):
         """ taking only the doa, elevation data and others not accurate
         """
-        self.pinger_data = msg
-        self.doa_values.append(msg.doa)
-        # self.get_logger().info(f'Received pinger data: DOA={msg.doa}')
-        if self.is_active:
-            self.process_data()
-    
-    def compute_average_doa(self):
-        """Compute the average of the stored DOA values."""
-        self.get_logger().info(f"DOA values: {self.doa_values}")
-        
-        if len(self.doa_values) < self.doa_cluster_size :
-            return -1  # Default value if no DOA values are available
-        return sum(self.doa_values) / len(self.doa_values)
-
-    def process_data(self):
-        """Process the latest data and publish gate status."""
+         # self.get_logger().info(f'Received pinger data: DOA={msg.doa}')
+  
         if not self.is_active:
-            return  # Do nothing if the node is not activated
-        
-        # if not self.gate_detection or not self.vehicle_position or not self.pinger_data:
-        #     self.get_logger().warn('Waiting for all sensor data...',throttle_duration_sec=2.0)
-        #     return
-        if not self.pinger_data:
             return 
+    
+        self.batch.append(self.get_gate_from_doa(msg.doa))
+        if len(self.batch) == self.doa_cluster_size:
+            gate = Counter(self.batch).most_common(1)[0][0]
+            self.get_logger().info(f"Gate is: {gate}")
             
+            gate_pub = String()
+            gate_pub.data = gate
+            self.pub_gate_.publish(gate_pub)
+            self.batch = []
+            
+     
+    # def compute_average_doa(self):
+    #     """Compute the average of the stored DOA values."""
+    #     self.get_logger().info(f"DOA values: {self.doa_values}")
+        
+    #     if len(self.doa_values) < self.doa_cluster_size :
+    #         return -1  # Default value if no DOA values are available
+    #     return sum(self.doa_values) / len(self.doa_values)
 
-        # Determine gate position using gpinger & gate data
-        # pinger_gate = self.determine_pinger_gate(self.gate_detection, self.vehicle_position, self.pinger_data)
-        pinger_gate = self.determine_pinger_gate()
-        confidence_ok = self.check_pinger_gate_status(self.pinger_data)
+    # def process_data(self):
+    #     if not self.is_active:
+    #         return 
+            
+    #     # pinger_gate = self.determine_pinger_gate(self.gate_detection, self.vehicle_position, self.pinger_data)
+    #     pinger_gate = self.determine_pinger_gate()
+    #     confidence_ok = self.check_pinger_gate_status(self.pinger_data)
 
-        # Publish the gate position as a string
-        pinger_gate_msg = String()
-        pinger_gate_msg.data = pinger_gate
-        self.pub_gate_.publish(pinger_gate_msg)
+    #     # Publish the gate position as a string
+    #     pinger_gate_msg = String()
+    #     pinger_gate_msg.data = pinger_gate
+    #     self.timer = self.create_timer(1.0 , self.pub_gate_.publish(pinger_gate_msg))
 
-        # Publish the confidence status as a boolean
-        confidence_msg = Bool()
-        confidence_msg.data = confidence_ok
-        self.pub_status_.publish(confidence_msg)
+    #     # Publish the confidence status as a boolean
+    #     confidence_msg = Bool()
+    #     confidence_msg.data = confidence_ok
+    #     self.pub_status_.publish(confidence_msg)
 
         # self.get_logger().info(f'Published: {pinger_gate}, Confidence OK: {confidence_ok}')
-
-    def determine_pinger_gate(self):
-        """ Assumes asv is in front of the gate & stationary """
-        """TODO : Check asv in front of the gate"""
-        avg_doa = self.compute_average_doa()
-        if avg_doa < 0: 
-            self.get_logger().warn(f"doa cluster not filled, average not accurate, aborting")
-            tmp = "Invalid"
-        elif 340 < avg_doa or avg_doa < 25 : 
-            tmp =  "gate_middle"
-        elif 25 <= avg_doa <= 90 : 
-            tmp = "gate_right"
-        elif 270 <= avg_doa <= 340 : 
-            tmp = "gate_left"
-        else :
-            self.get_logger().warn(f"Invalid DOA, gate not determined")
-            tmp = "Invalid"
-        self.get_logger().info(f"Gate is: {tmp}")
-        return tmp
 
     def check_pinger_gate_status(self, pinger_data):
         """TODO"""
