891077f8de156553901f55d075e9db010db61a30
SamuelFoo <fooenzesamuel@gmail.com>
2025-05-19 03:54:06 +0800

    Add pose clustering node

---
+++ bb_filters/cluster.py
@@ -0,0 +1,111 @@
+from operator import attrgetter
+from typing import List, Tuple
+
+import numpy as np
+from geometry_msgs.msg import PoseWithCovarianceStamped
+from rclpy.impl.rcutils_logger import RcutilsLogger
+from sklearn.cluster import HDBSCAN
+
+
+def get_position_tuple_from_pose(
+    pose: PoseWithCovarianceStamped,
+) -> Tuple[float, float, float]:
+    """Get the position tuple from a PoseWithCovarianceStamped message.
+
+    Args:
+        pose (PoseWithCovarianceStamped): The pose message.
+
+    Returns:
+        tuple(float, float, float): The position tuple.
+    """
+    return attrgetter("x", "y", "z")(pose.pose.pose.position)
+
+
+def get_orientation_tuple_from_pose(
+    pose: PoseWithCovarianceStamped,
+) -> Tuple[float, float, float, float]:
+    """Get the orientation tuple from a PoseWithCovarianceStamped message.
+
+    Args:
+        pose (PoseWithCovarianceStamped): The pose message.
+
+    Returns:
+        tuple(float, float, float, float): The orientation tuple.
+    """
+    return attrgetter("x", "y", "z", "w")(pose.pose.pose.orientation)
+
+
+def get_covariance_from_pose(
+    pose: PoseWithCovarianceStamped,
+) -> np.ndarray:
+    """Get the covariance matrix from a PoseWithCovarianceStamped message.
+
+    Args:
+        pose (PoseWithCovarianceStamped): The pose message.
+
+    Returns:
+        np.ndarray: The covariance matrix.
+    """
+    return np.array(pose.pose.covariance).reshape(6, 6)
+
+
+def get_average_pose(
+    pose_msgs: List[PoseWithCovarianceStamped],
+    logger: RcutilsLogger = None,
+) -> PoseWithCovarianceStamped:
+    """Get the average pose from a list of PoseWithCovarianceStamped messages.
+
+    Warning! The quaternion of the last pose is returned if orientation averaging fails.
+
+    Args:
+        pose_msgs (List[PoseWithCovarianceStamped]): The list of pose messages.
+
+    Returns:
+        PoseWithCovarianceStamped: The average pose message.
+    """
+    avg_pose = PoseWithCovarianceStamped()
+
+    positions = np.array([get_position_tuple_from_pose(pose) for pose in pose_msgs])
+    centroid = positions.mean(axis=0)
+    avg_pose.pose.pose.position.x = centroid[0]
+    avg_pose.pose.pose.position.y = centroid[1]
+    avg_pose.pose.pose.position.z = centroid[2]
+
+    try:
+        quats = np.array([get_orientation_tuple_from_pose(pose) for pose in pose_msgs])
+        quat_matrix = np.dot(quats.T, quats)
+        eigvals, eigvecs = np.linalg.eigh(quat_matrix)
+        avg_quat = eigvecs[:, np.argmax(eigvals)]  # eigenvector with largest eigenvalue
+    except np.linalg.LinAlgError:
+        avg_quat = pose_msgs[-1].pose.pose.orientation
+        if logger:
+            logger.warning(
+                "Quaternion averaging failed, using the last pose's quaternion."
+            )
+
+    avg_pose.pose.pose.orientation.x = avg_quat[0]
+    avg_pose.pose.pose.orientation.y = avg_quat[1]
+    avg_pose.pose.pose.orientation.z = avg_quat[2]
+    avg_pose.pose.pose.orientation.w = avg_quat[3]
+
+    covariances = np.array([get_covariance_from_pose(pose) for pose in pose_msgs])
+    avg_covariance = np.mean(covariances, axis=0)
+    avg_pose.pose.covariance = avg_covariance.flatten().tolist()
+
+    return avg_pose
+
+
+def get_idxs_in_largest_cluster(
+    hdbscan: HDBSCAN,
+    positions: np.ndarray,
+) -> np.ndarray:
+    """Returns an array of indices belonging to the largest cluster."""
+    hdbscan.fit(positions)
+
+    labels = np.array(hdbscan.labels_)
+    non_noise_labels = labels[labels >= 0]
+    unique_labels, unique_label_counts = np.unique(non_noise_labels, return_counts=True)
+    largest_cluster_label = unique_labels[np.argmax(unique_label_counts)]
+    largest_cluster_idxs = np.where(labels == largest_cluster_label)[0]
+
+    return largest_cluster_idxs
---
+++ scripts/cluster_poses_node.py
@@ -0,0 +1,158 @@
+#!/usr/bin/env python3
+from typing import List
+
+import numpy as np
+import rclpy
+from geometry_msgs.msg import PoseArray, PoseWithCovarianceStamped
+from message_filters import Cache, Subscriber
+from rcl_interfaces.msg import ParameterDescriptor
+from rclpy.node import Node
+from sklearn.cluster import HDBSCAN
+
+from bb_filters.cluster import (
+    get_average_pose,
+    get_idxs_in_largest_cluster,
+    get_position_tuple_from_pose,
+)
+
+
+class ClusterPosesNode(Node):
+    def __init__(self):
+        super().__init__("cluster_poses_node")
+
+        self.declare_parameter(
+            "input_pose_topic",
+            "input_pose",
+            ParameterDescriptor(
+                description="Input ROS topic for PoseWithCovarianceStamped messages."
+            ),
+        )
+        self.declare_parameter(
+            "output_pose_topic",
+            "output_pose",
+            ParameterDescriptor(
+                description="Output ROS topic for filtered PoseWithCovarianceStamped."
+            ),
+        )
+        self.declare_parameter(
+            "output_pose_array_topic",
+            "output_pose_array",
+            ParameterDescriptor(
+                description="Output ROS topic for array of input poses fed to clustering. For debugging."
+            ),
+        )
+        self.declare_parameter(
+            "cluster_interval",
+            1.0,
+            ParameterDescriptor(
+                description="Time interval in seconds between computations of filtered pose."
+            ),
+        )
+        self.declare_parameter(
+            "num_poses",
+            20,
+            ParameterDescriptor(description="Number of poses to cache."),
+        )
+        self.declare_parameter(
+            "min_cluster_size",
+            2,
+            ParameterDescriptor(description="Argument for sklearn's HDBSCAN."),
+        )
+        self.declare_parameter(
+            "min_samples",
+            1,
+            ParameterDescriptor(description="Argument for sklearn's HDBSCAN."),
+        )
+
+        input_pose_topic = (
+            self.get_parameter("input_pose_topic").get_parameter_value().string_value
+        )
+        output_pose_topic = (
+            self.get_parameter("output_pose_topic").get_parameter_value().string_value
+        )
+        output_pose_array_topic = (
+            self.get_parameter("output_pose_array_topic")
+            .get_parameter_value()
+            .string_value
+        )
+        cluster_interval = (
+            self.get_parameter("cluster_interval").get_parameter_value().double_value
+        )
+        num_poses = self.get_parameter("num_poses").get_parameter_value().integer_value
+        self.min_cluster_size = (
+            self.get_parameter("min_cluster_size").get_parameter_value().integer_value
+        )
+        self.min_samples = (
+            self.get_parameter("min_samples").get_parameter_value().integer_value
+        )
+
+        self.pose_subscriber = Subscriber(
+            self, PoseWithCovarianceStamped, input_pose_topic
+        )
+        self.pose_cache = Cache(self.pose_subscriber, cache_size=num_poses)
+        self.pose_publisher = self.create_publisher(
+            PoseWithCovarianceStamped, output_pose_topic, 10
+        )
+        self.pose_array_publisher = self.create_publisher(
+            PoseArray, output_pose_array_topic, 10
+        )
+        self.create_timer(
+            cluster_interval,
+            self.cluster_timer_callback,
+        )
+
+        self.hdbscan = HDBSCAN(
+            min_cluster_size=self.min_cluster_size,
+            min_samples=self.min_samples,
+            cluster_selection_epsilon=0.05,
+            allow_single_cluster=True,
+            store_centers="centroid",
+        )
+
+    def cluster_timer_callback(self):
+        oldest_time = self.pose_cache.getOldestTime()
+        latest_time = self.pose_cache.getLastestTime()
+
+        if oldest_time is None or latest_time is None:
+            self.get_logger().warn("No poses in cache.")
+            return
+
+        pose_msgs: List[PoseWithCovarianceStamped] = self.pose_cache.getInterval(
+            oldest_time, latest_time
+        )
+
+        min_num_poses = max(self.min_cluster_size, self.min_samples)
+        if len(pose_msgs) < min_num_poses:
+            self.get_logger().warn(
+                f"Not enough poses to cluster. Received: {len(pose_msgs)}. Required: {min_num_poses}."
+            )
+            return
+
+        # Publish the array of poses for debugging
+        pose_array_msg = PoseArray()
+        pose_array_msg.header = pose_msgs[-1].header
+        pose_array_msg.poses = [pose.pose.pose for pose in pose_msgs]
+        self.pose_array_publisher.publish(pose_array_msg)
+
+        positions = np.array([get_position_tuple_from_pose(pose) for pose in pose_msgs])
+        filtered_idxs = get_idxs_in_largest_cluster(self.hdbscan, positions)
+        filtered_poses = [pose_msgs[i] for i in filtered_idxs]
+
+        avg_pose = get_average_pose(filtered_poses, self.get_logger())
+        latest_msg: PoseWithCovarianceStamped = self.pose_cache.getLast()
+        avg_pose.header = latest_msg.header
+        self.pose_publisher.publish(avg_pose)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = ClusterPosesNode()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+    rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
