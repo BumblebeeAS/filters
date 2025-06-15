b35ccf7601266649aec3d9688074be5a0b7908fd
advaypakhale <advay.pakhale@gmail.com>
2025-06-15 13:58:35 +0800

    feat: add gate identification to slalom cluster server
    - refactor: separate out tf lru cache

--- scripts/cluster_tf_action_server.py
+++ scripts/cluster_tf_action_server.py
@@ -13,6 +13,8 @@ from rclpy.node import Node
 from rclpy.time import Time
 from sklearn.cluster import HDBSCAN
 
+from bb_filters.tf_lru_cache import TfLruCache
+
 
 class ClusterTfActionServer(Node):
     """
@@ -304,75 +306,6 @@ class ClusterTfActionServer(Node):
 
         goal_handle.succeed()
         return result
-
-
-class TfLruCache:
-    def __init__(self, size: int, logger):
-        self.size = size
-
-        # idx is the current insertion index (the open spot in the circular buffer)
-        self.idx = 0
-
-        self.cache = [None] * self.size
-        self.logger = logger
-
-        self.oldest_time = Time()
-        self.latest_time = Time()
-
-        self.is_empty_flag = True
-        self.count = 0  # number of elements in cache
-
-    @property
-    def is_full(self) -> bool:
-        return self.count >= self.size
-
-    def _get(self, idx: int) -> TransformStamped:
-        return self.cache[idx % self.size]
-
-    def _set(self, tf: TransformStamped):
-        self.cache[self.idx] = tf
-        self.latest_time = Time.from_msg(tf.header.stamp)
-        self.idx = (self.idx + 1) % self.size
-        self.count += 1
-
-    def add(self, tf: TransformStamped):
-        if self.is_empty_flag:
-            self.oldest_time = Time.from_msg(tf.header.stamp)
-            self.is_empty_flag = False
-            self._set(tf)
-            return True
-
-        prev_tf = self._get(self.idx - 1)
-
-        if Time.from_msg(tf.header.stamp) == Time.from_msg(prev_tf.header.stamp):
-            self.logger.warn(
-                f"Skipping TF with timestamp {tf.header.stamp} as it is the same as the previous one."
-            )
-            return False
-
-        if self.is_full:
-            self.oldest_time = Time.from_msg(self._get(self.idx + 1).header.stamp)
-
-        self._set(tf)
-        return True
-
-    def get_oldest_time(self) -> Time:
-        return self.oldest_time
-
-    def get_latest_time(self) -> Time:
-        return self.latest_time
-
-    def is_empty(self) -> bool:
-        return self.is_empty_flag
-
-    def get_all(self) -> tuple[list[TransformStamped], Time]:
-        return (
-            [self.cache[i] for i in range(self.size) if self.cache[i] is not None],
-            self.get_latest_time(),
-        )
-
-    def get_count(self) -> int:
-        return self.count
 
 
 def main(args=None):
--- scripts/robosub25/slalom_cluster_action_server.py
+++ scripts/robosub25/slalom_cluster_action_server.py
@@ -4,7 +4,6 @@ import traceback
 import numpy as np
 import rclpy
 import tf2_ros
-from bb_filters.cluster import tf_to_pose_stamped
 from bb_perception_msgs.action import ClusterTf
 from geometry_msgs.msg import Pose, PoseArray, Quaternion, TransformStamped, Vector3
 from rclpy.action import ActionServer, CancelResponse, GoalResponse
@@ -13,6 +12,9 @@ from rclpy.executors import MultiThreadedExecutor
 from rclpy.node import Node
 from rclpy.time import Time
 from sklearn.cluster import HDBSCAN
+
+from bb_filters.cluster import tf_to_pose_stamped
+from bb_filters.tf_lru_cache import TfLruCache
 
 
 class SlalomClusterActionServer(Node):
@@ -318,22 +320,56 @@ class SlalomClusterActionServer(Node):
 
         average_transforms = self.order_transforms(tfs, top_3_indices, curr_pos)
 
-        for i, output_child in enumerate(
-            [
-                self.channel_one_tf_clustered,
-                self.channel_two_tf_clustered,
-                self.channel_three_tf_clustered,
-            ]
-        ):
-            if i >= len(average_transforms):
-                break
+        # if zero missing
+        if len(average_transforms) == 3:
+            for i, output_child in enumerate(
+                [
+                    self.channel_one_tf_clustered,
+                    self.channel_two_tf_clustered,
+                    self.channel_three_tf_clustered,
+                ]
+            ):
+                self.publish_transform(
+                    translation=average_transforms[i][0],
+                    rotation=average_transforms[i][1],
+                    latest_time=latest_time,
+                    output_child=output_child,
+                )
+        # if one missing
+        elif len(average_transforms) == 2:
+            # check the offset
+            delta_z = abs(average_transforms[0][0].z - average_transforms[1][0].z)
 
             self.publish_transform(
-                translation=average_transforms[i][0],
-                rotation=average_transforms[i][1],
+                translation=average_transforms[0][0],
+                rotation=average_transforms[0][1],
                 latest_time=latest_time,
-                output_child=output_child,
+                output_child=self.channel_one_tf_clustered,
             )
+            # likely gates 0 and 2
+            if delta_z > 3.0:
+                self.publish_transform(
+                    translation=average_transforms[1][0],
+                    rotation=average_transforms[1][1],
+                    latest_time=latest_time,
+                    output_child=self.channel_three_tf_clustered,
+                )
+            else:
+                self.publish_transform(
+                    translation=average_transforms[1][0],
+                    rotation=average_transforms[1][1],
+                    latest_time=latest_time,
+                    output_child=self.channel_two_tf_clustered,
+                )
+        elif len(average_transforms) == 1:
+            self.publish_transform(
+                translation=average_transforms[0][0],
+                rotation=average_transforms[0][1],
+                latest_time=latest_time,
+                output_child=self.channel_one_tf_clustered,
+            )
+        else:
+            self.get_logger().warn("No clusters found!")
 
         # feedback_msg.current_status = "Clustering complete, static transform published"
         # goal_handle.publish_feedback(feedback_msg)
@@ -415,75 +451,6 @@ class SlalomClusterActionServer(Node):
         pose.position.z = tf.transform.translation.z
         pose.orientation = tf.transform.rotation
         return pose
-
-
-class TfLruCache:
-    def __init__(self, size: int, logger):
-        self.size = size
-
-        # idx is the current insertion index (the open spot in the circular buffer)
-        self.idx = 0
-
-        self.cache = [None] * self.size
-        self.logger = logger
-
-        self.oldest_time = Time()
-        self.latest_time = Time()
-
-        self.is_empty_flag = True
-        self.count = 0  # number of elements in cache
-
-    @property
-    def is_full(self) -> bool:
-        return self.count >= self.size
-
-    def _get(self, idx: int) -> TransformStamped:
-        return self.cache[idx % self.size]
-
-    def _set(self, tf: TransformStamped):
-        self.cache[self.idx] = tf
-        self.latest_time = Time.from_msg(tf.header.stamp)
-        self.idx = (self.idx + 1) % self.size
-        self.count += 1
-
-    def add(self, tf: TransformStamped):
-        if self.is_empty_flag:
-            self.oldest_time = Time.from_msg(tf.header.stamp)
-            self.is_empty_flag = False
-            self._set(tf)
-            return True
-
-        prev_tf = self._get(self.idx - 1)
-
-        if Time.from_msg(tf.header.stamp) == Time.from_msg(prev_tf.header.stamp):
-            self.logger.warn(
-                f"Skipping TF with timestamp {tf.header.stamp} as it is the same as the previous one."
-            )
-            return False
-
-        if self.is_full:
-            self.oldest_time = Time.from_msg(self._get(self.idx + 1).header.stamp)
-
-        self._set(tf)
-        return True
-
-    def get_oldest_time(self) -> Time:
-        return self.oldest_time
-
-    def get_latest_time(self) -> Time:
-        return self.latest_time
-
-    def is_empty(self) -> bool:
-        return self.is_empty_flag
-
-    def get_all(self) -> tuple[list[TransformStamped], Time]:
-        return (
-            [self.cache[i] for i in range(self.size) if self.cache[i] is not None],
-            self.get_latest_time(),
-        )
-
-    def get_count(self) -> int:
-        return self.count
 
 
 def main(args=None):
