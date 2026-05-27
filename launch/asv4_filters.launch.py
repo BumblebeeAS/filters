daeb50b8afce7e35a3a4ae39a2892c567695d507
SamuelFoo <fooenzesamuel@gmail.com>
2026-05-27 02:44:46 +0200

    refactor: remove unused visualization and cleanup modules

--- launch/asv4_filters.launch.py
+++ launch/asv4_filters.launch.py
@@ -7,82 +7,11 @@ from ament_index_python.packages import get_package_share_directory
 
 # Launch File for running the lidar segmentation pipeline on the BBASV4 Simulation
 
-detected_object_3d_vis = [
-    "/asv4/vision/lidar_small_objects/dets_3d",
-    "/asv4/vision/lidar_small_objects/dets_3d/filtered",
-    "/asv4/vision/lidar_small_objects/dets_3d/labelled",
-    # "/asv4/vision/lidar_large_objects/dets_3d",
-    # "/asv4/vision/lidar_large_objects/dets_3d/filtered",
-    # "/asv4/vision/lidar_large_objects/dets_3d/labelled",
-    "/asv4/bev_detections/filtered",
-    "/asv4/tasks/scan_dock_deliver/placard/detections_3d",
-    "/asv4/vision/gate_detections",
-    "/asv4/vision/detections_2d/projected",
-    "/asv4/vision/red_green_gate_detections"
-]
-detected_object_3d_vis_tf = [
-    "/asv4/robotx/filtered_detections",
-]
-detected_object_2d_vis = [
-    "/asv4/vision/detections_2d",
-    "/asv4/vision/detections_2d/fixed"
-]
-
 
 def generate_launch_description():
     return LaunchDescription(
         [
             # ComposableNodeContainer(),
-
-            *[
-                Node(
-                    package="bb_filters",
-                    executable="detected_object_3d_vis",
-                    name=f"{topic.replace('/', '_')}_vis",
-                    parameters=[
-                        {
-                            "input_detections_topics": [topic],
-                            "output_markers_topic": f"{topic}/marker",
-                            "objects_config": "robotx.yaml",
-                        }
-                    ],
-                ) for topic in detected_object_3d_vis
-            ],
-            *[
-                Node(
-                    package="bb_filters",
-                    executable="detected_object_3d_vis",
-                    name=f"{topic.replace('/', '_')}_vis",
-                    parameters=[
-                        {
-                            "input_detections_topics": [topic],
-                            "output_markers_topic": f"{topic}/marker",
-                            "objects_config": "robotx.yaml",
-                            "publish_tf": True,
-                            "use_sim_time": True,
-                        }
-                    ],
-                ) for topic in detected_object_3d_vis_tf
-            ],
-            *[
-                Node(
-                    package="bb_filters",
-                    executable="detected_object_2d_vis",
-                    name=f"{topic.replace('/', '_')}_vis",
-                    parameters=[
-                        {
-                            "input_detections_topics": [topic],
-                            "camera_info_topics": [
-                                "/asv4/left_cam/camera_info",
-                                "/asv4/right_cam/camera_info",
-                                "/asv4/front_cam/camera_info",
-                            ],
-                            "output_markers_topic": f"{topic}/marker",
-                            "objects_config": "robotx.yaml",
-                        }
-                    ],
-                ) for topic in detected_object_2d_vis
-            ],
             Node(
                 package="bb_filters",
                 executable="detected_object_3d_filter.py",
--- launch/asv4_filters_sim.launch.py
+++ launch/asv4_filters_sim.launch.py
@@ -4,77 +4,10 @@ from launch_ros.actions import Node
 # Launch File for running the lidar segmentation pipeline on the BBASV4 Simulation
 
 
-detected_object_3d_vis = [
-    "/asv4/vision/lidar_small_objects/dets_3d",
-    "/asv4/vision/lidar_small_objects/dets_3d/filtered",
-    "/asv4/vision/lidar_small_objects/dets_3d/labelled",
-    "/asv4/bev_detections/filtered",
-    "/asv4/tasks/scan_dock_deliver/placard/detections_3d",
-    "/asv4/vision/gate_detections",
-    "/asv4/vision/detections_2d/projected",
-    "/asv4/vision/red_green_gate_detections"
-]
-detected_object_3d_vis_tf = [
-    "/asv4/robotx/filtered_detections",
-]
-detected_object_2d_vis = [
-    "/asv4/vision/detections_2d",
-    "/asv4/vision/detections_2d/fixed"
-]
-
 def generate_launch_description():
     return LaunchDescription(
         [
             # ComposableNodeContainer(),
-            *[
-                Node(
-                    package="bb_filters",
-                    executable="detected_object_3d_vis",
-                    name=f"{topic.replace('/', '_')}_vis",
-                    parameters=[
-                        {
-                            "input_detections_topics": [topic],
-                            "output_markers_topic": f"{topic}/marker",
-                            "objects_config": "robotx.yaml",
-                        }
-                    ],
-                ) for topic in detected_object_3d_vis
-            ],
-            *[
-                Node(
-                    package="bb_filters",
-                    executable="detected_object_3d_vis",
-                    name=f"{topic.replace('/', '_')}_vis",
-                    parameters=[
-                        {
-                            "input_detections_topics": [topic],
-                            "output_markers_topic": f"{topic}/marker",
-                            "objects_config": "robotx.yaml",
-                            "publish_tf": True,
-                            "use_sim_time": True,
-                        }
-                    ],
-                ) for topic in detected_object_3d_vis_tf
-            ],
-            *[
-                Node(
-                    package="bb_filters",
-                    executable="detected_object_2d_vis",
-                    name=f"{topic.replace('/', '_')}_vis",
-                    parameters=[
-                        {
-                            "input_detections_topics": [topic],
-                            "camera_info_topics": [
-                                "/asv4/left_cam/camera_info",
-                                "/asv4/right_cam/camera_info",
-                                "/asv4/front_cam/camera_info",
-                            ],
-                            "output_markers_topic": f"{topic}/marker",
-                            "objects_config": "robotx.yaml",
-                        }
-                    ],
-                ) for topic in detected_object_2d_vis
-            ],
             Node(
                 package="bb_filters",
                 executable="detected_object_2d_filter_projection.py",
@@ -174,36 +107,6 @@ def generate_launch_description():
             # ),
             # Node(
             #     package="bb_filters",
-            #     executable="detected_object_3d_vis",
-            #     name="large_filtered_dets_vis",
-            #     parameters=[
-            #         {
-            #             "input_detections_topics": [
-            #                 "/asv4/vision/lidar_large_objects/dets_3d/labelled",
-            #             ],
-            #             "output_markers_topic": "/asv4/vision/lidar_large_objects/dets_3d/filtered/marker",
-            #             "objects_config": "robotx.yaml",
-            #             "publish_tf": False,
-            #         }
-            #     ],
-            # ),
-            # Node(
-            #     package="bb_filters",
-            #     executable="detected_object_3d_vis",
-            #     name="large_labelled_dets_vis",
-            #     parameters=[
-            #         {
-            #             "input_detections_topics": [
-            #                 "/asv4/vision/lidar_large_objects/dets_3d/labelled",
-            #             ],
-            #             "output_markers_topic": "/asv4/vision/lidar_large_objects/dets_3d/labelled/marker",
-            #             "objects_config": "robotx.yaml",
-            #             "publish_tf": False,
-            #         }
-            #     ],
-            # ),
-            # Node(
-            #     package="bb_filters",
             #     executable="detected_object_3d_labelling.py",
             #     name="large_det_3d_labeller",
             #     parameters=[
@@ -217,21 +120,6 @@ def generate_launch_description():
             #             ],
             #             "output_labeled_topic": "/asv4/vision/lidar_large_objects/dets_3d/labelled",
             #             "objects_config": "robotx.yaml",
-            #         }
-            #     ],
-            # ),
-            # Node(
-            #     package="bb_filters",
-            #     executable="detected_object_3d_vis",
-            #     name="large_labelled_dets_vis",
-            #     parameters=[
-            #         {
-            #             "input_detections_topics": [
-            #                 "/asv4/vision/lidar_large_objects/dets_3d/labelled",
-            #             ],
-            #             "output_markers_topic": "/asv4/vision/lidar_large_objects/dets_3d/labelled/marker",
-            #             "objects_config": "robotx.yaml",
-            #             "publish_tf": False,
             #         }
             #     ],
             # ),
